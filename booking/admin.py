a4fac203a67ef1fa44205431ce8365876d9bb3c7
Tomasz Słupik <slupiktomasz@gmail.com>
2024-12-18 08:05:52 +0100

    Zmiana w panelu admina

--- booking/admin.py
+++ booking/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Booking
+from django.utils import formats
 
 # Register your models here.
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ("name_car_booking", "image_car", "is_booked")
+    list_display = ("name_car_booking", "image_car", "is_booked", "formated_start_date", "formated_end_date", "user")
+
+
+    def formated_start_date(self, object):
+        return formats.date_format(object.start_date, "SHORT_DATE_FORMAT") if object.start_date else "Brak daty rezerwacji"
+    
+    def formated_end_date(self, object):
+        return formats.date_format(object.end_date, "SHORT_DATE_FORMAT") if object.end_date else "Brak daty rezerwacji"
