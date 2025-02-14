1771f45effc3450b635a85897ba66a9d1aea1bcf
Tomasz Słupik <slupiktomasz@gmail.com>
2025-02-14 19:03:40 +0100

    Dodanie JS

--- booking/static/js/booking.js
+++ booking/static/js/booking.js
@@ -1,6 +1,24 @@
 const reservationButtons = document.querySelectorAll('.btnReservation');
 const resert_filter = document.querySelector('.reset_filter')
 
+document.addEventListener('DOMContentLoaded', function() {
+    const accessToken = localStorage.getItem('access'); 
+
+    if (!accessToken) {
+        console.error("Brak tokenu dostępu. Użytkownik może nie być zalogowany.");
+        Swal.fire({
+            title: 'Błąd!',
+            text: 'Musisz być zalogowany, aby korzystać z aplikacji',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = 'http://127.0.0.1:8000/';
+            }
+        });
+        return;  
+    }
+})
 
 // Resetowanie filtra:
 const resetAllCar= () => {
